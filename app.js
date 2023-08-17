69a78ba2afda5a9101459b0ad6ee12767a62de37
AASJATT <141618407+AASJATT@users.noreply.github.com>
2023-08-17 14:01:14 +0000

    queryselector-callback function

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var username = prompt("Give me username")
-var welcomeMessage="this script works " + username
-alert(welcomeMessage);
+var btnTranslate = document.querySelector("#btn-translate")
 
-
+btnTranslate.addEventListener("click", clickHandler)
+    
+function clickHandler()
+{
+    console.log("clicked!");
+};
